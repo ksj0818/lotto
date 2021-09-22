1f85f7fb590f50fe1d8e05ba8660af913d80dd57
ksj0818 <ksj071808@gmail.com>
2021-09-22 23:34:55 +0900

    Update index.js
    로또볼 버튼컬러 설정 및 디스플레이 정렬

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,6 @@
 const lottoNumberBtn = document.getElementById('lotto-numberBtn');
 const lottoBall = document.getElementsByClassName('lotto-ball')
-
+const plusIcon = document.getElementById('plus-icon');
 // 로또 번호 1~45 세팅
 let lottoList = [];
 // 로또 리스트에서 랜덤으로 뽑은 번호를 넣어줄 배열
@@ -8,6 +8,7 @@ let luckyNumberList = [];
 // 마지막에 뽑을 서비스 번호 담을 변수
 let serviceNumber;
 
+plusIcon.style.display = "none";
 
 lottoNumberBtn.addEventListener('click', function() { 
   createLottoNumbers();
@@ -18,6 +19,7 @@ lottoNumberBtn.addEventListener('click', function() {
   luckyNumberList.push(Number(serviceNumber));
   
   fetchList();
+  plusIcon.style.display = "block";
 });
 
 // functions
@@ -47,3 +49,19 @@ function randomNumbers(count) {
   return returnLuckyNumber;
 }
 
+function fetchList() {
+  for(let i = 0; i < lottoBall.length; i++) {
+    lottoBall[i].innerHTML = luckyNumberList[i];
+    if (luckyNumberList[i] >= 1 && luckyNumberList[i] <= 9) {
+      lottoBall[i].className = 'btn btn-warning lotto-ball';
+    } else if (luckyNumberList[i] >= 10 && luckyNumberList[i] <= 19) {
+      lottoBall[i].className = 'btn btn-primary lotto-ball';
+    } else if (luckyNumberList[i] >= 20 && luckyNumberList[i] <= 29) {
+      lottoBall[i].className = 'btn btn-danger lotto-ball';
+    } else if (luckyNumberList[i] >= 30 && luckyNumberList[i] <= 39) {
+      lottoBall[i].className = 'btn btn-secondary lotto-ball';
+    } else if (luckyNumberList[i] >= 40 && luckyNumberList[i] <= 45) {
+      lottoBall[i].className = 'btn btn-success lotto-ball';
+    }
+  }
+}
